081a0924ee33c9ded2c5d61af27ba0096dd02e16
maiza <abdelmaiza33@gmail.com>
2023-01-19 00:09:41 +0100

    add new app

---
+++ infra/src/main/java/com/cirta/main/BatchApp.java
@@ -0,0 +1,10 @@
+package com.cirta.main;
+
+import software.amazon.awscdk.App;
+
+public class BatchApp {
+    public static void main(String[] args) {
+        App app = new App();
+        app.synth();
+    }
+}
